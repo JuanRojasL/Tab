1eca6b7e4087afec07a06187ab9db37fbca4f42e
JuanRojasL <112908543+JuanRojasL@users.noreply.github.com>
2022-10-25 23:47:39 -0500

    Add files via upload

---
+++ Tab2/app/src/main/java/com/example/tab/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Fragment1(),"CHATS");
+        vpAdapter.addFragment(new Fragment2(),"STATUS");
+        vpAdapter.addFragment(new Fragment3(),"CALLS");
+        viewPager.setAdapter(vpAdapter);
+    }
+}
